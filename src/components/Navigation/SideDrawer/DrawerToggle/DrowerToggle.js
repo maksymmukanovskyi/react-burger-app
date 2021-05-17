4e1b57f059731a0c13de15e3d4bdaf057f231d53
Maksym Mukanovskyi <maksymmukanovskyi@gmail.com>
2021-05-16 20:13:09 -0600

    added_css_to_sidedrawer

--- src/components/Navigation/SideDrawer/DrawerToggle/DrowerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrowerToggle.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import classes from './DrowerToggle.module.css';
 
 const DrowerToggle = props => {
   const { clicked } = props;
   return (
     // eslint-disable-next-line jsx-a11y/no-static-element-interactions
-    <div type="button" onClick={clicked}>
-      MENU
+    <div className={classes.DrawerToggle} type="button" onClick={clicked}>
+      <div />
+      <div />
+      <div />
     </div>
   );
 };
