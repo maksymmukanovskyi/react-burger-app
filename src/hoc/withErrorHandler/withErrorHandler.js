7b83fd6c83b753e715663ca54413f8f8073b55b2
Maksym Mukanovskyi <maksymmukanovskyi@gmail.com>
2021-06-07 17:31:16 -0600

    removing interceptors on conponent will unmount

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -9,16 +9,21 @@ const withErrorHandler = (WrappedComponent, axios) => {
     };
 
     componentDidMount() {
-      axios.interceptors.request.use(req => {
+      this.reqInerceptor = axios.interceptors.request.use(req => {
         this.setState({ error: null });
         return req;
       });
-      axios.interceptors.response.use(
+      this.resInerceptor = axios.interceptors.response.use(
         res => res,
         error => {
           this.setState({ error });
         },
       );
+    }
+
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInerceptor);
+      axios.interceptors.response.eject(this.resInerceptor);
     }
 
     errorConfirmedHandler = () => {
